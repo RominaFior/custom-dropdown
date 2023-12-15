34fa9ea038c3157365f4c1185f72c27b39b9c087
Romina <fior.romina4@gmail.com>
2023-12-15 17:11:43 +0000

    removed comment

--- src/app/dropdown/services/dropdown.component.service.ts
+++ src/app/dropdown/services/dropdown.component.service.ts
@@ -1,6 +1,5 @@
-// dropdown.service.ts
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
